6f3ff96868e845c3b42522b2d754049d075e26b8
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-10-06 22:46:41 +0500

    3

---
+++ 1662-check-if-two-string-arrays-are-equivalent.swift
@@ -0,0 +1,16 @@
+/**
+ * 1662. Check If Two String Arrays are Equivalent
+ * https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+ */
+
+ class Solution {
+     func arrayStringsAreEqual(_ word1: [String], _ word2: [String]) -> Bool {
+         return word1.reduce("", +) == word2.reduce("", +)
+     }
+ }
+
+class Solution {
+    func arrayStringsAreEqual(_ word1: [String], _ word2: [String]) -> Bool {
+        return word1.joined() == word2.joined()
+    }
+}
